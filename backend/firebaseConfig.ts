cf0f1f5a7fe15082c0a1847d80fcefafe75369ae
Christian Rosenville <web.chrisr@gmail.com>
2024-10-20 23:05:29 +0200

    new firebase project

--- backend/firebaseConfig.ts
+++ backend/firebaseConfig.ts
@@ -1,19 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDwqE4czX2gQPelhGZdDcYvUvzuAeB1qIA",
-  authDomain: "lootlog-1c993.firebaseapp.com",
-  projectId: "lootlog-1c993",
-  storageBucket: "lootlog-1c993.appspot.com",
-  messagingSenderId: "27854388694",
-  appId: "1:27854388694:web:a8620ee185df2e35a87877",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_APP_ID,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
