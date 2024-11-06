1fd6305545536a296ad5a89e91bb47dc9b194cc2
Christian Rosenville <web.chrisr@gmail.com>
2024-11-06 21:03:11 +0100

    test

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,39 +1,36 @@
 /** @type {import('next').NextConfig} */
 
-// const withBundleAnalyzer = require("@next/bundle-analyzer")({
-//   enabled: process.env.ANALYZE === "true",
-// });
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "youtube.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "firebasestorage.googleapis.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "via.placeholder.com",
-//       },
-//     ],
-//   },
-//   webpack: (config) => {
-//     config.watchOptions = {
-//       poll: 1000,
-//       aggregateTimeout: 300,
-//     };
-//     return config;
-//   },
-// };
+const nextDevConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "youtube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
 
-// module.exports = withBundleAnalyzer(nextConfig);
-
-// PROD:
-module.exports = {
+const nextProdConfig = {
   output: "standalone",
   images: {
     remotePatterns: [
@@ -52,3 +49,9 @@ module.exports = {
     ],
   },
 };
+
+// PROD:
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? nextProdConfig
+    : withBundleAnalyzer(nextDevConfig);
