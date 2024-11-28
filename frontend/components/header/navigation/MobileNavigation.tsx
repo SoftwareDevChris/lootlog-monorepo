a8b2ed81ae340377e2d3661963712d540aec0198
Christian Rosenville <web.chrisr@gmail.com>
2024-11-28 22:26:52 +0100

    added outside click listener to the mobile navigation

--- frontend/components/header/navigation/MobileNavigation.tsx
+++ frontend/components/header/navigation/MobileNavigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 
 import { ROUTES } from "@/utils/routes";
@@ -8,10 +8,27 @@ import { ROUTES } from "@/utils/routes";
 import { HiMenu } from "react-icons/hi";
 import { IoCloseOutline } from "react-icons/io5";
 
-import { OutsideClickContainer } from "@/components/outsideClick/OutsideClick";
-
 export const MobileNavigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    if (isMenuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isMenuOpen, setIsMenuOpen]);
 
   return (
     <>
@@ -30,27 +47,23 @@ export const MobileNavigation = () => {
           />
         )}
 
-        <OutsideClickContainer
-          onClose={() => setIsMenuOpen(false)}
-          isOpen={isMenuOpen}
+        <ul
+          ref={menuRef}
+          style={isMenuOpen ? { maxHeight: "300px" } : { maxHeight: "0px" }}
+          className={`absolute left-0 top-full z-[99] mt-1 flex w-full flex-col items-center gap-6 overflow-hidden bg-neutral-800 transition-[max-height] duration-300 ease-linear`}
         >
-          <ul
-            style={isMenuOpen ? { maxHeight: "500px" } : {}}
-            className={`absolute left-0 top-full z-[99] mt-1 flex max-h-0 w-full flex-col items-center gap-6 overflow-hidden bg-neutral-800 transition-all duration-300 ease-linear`}
-          >
-            {ROUTES.map((route) => (
-              <li
-                className="cursor-pointer rounded-md first:mt-4 last:mb-4"
-                onClick={() => setIsMenuOpen(false)}
-                key={route.name}
-              >
-                <Link className="hover:underline" href={route.path}>
-                  {route.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </OutsideClickContainer>
+          {ROUTES.map((route) => (
+            <li
+              className="cursor-pointer rounded-md first:mt-4 last:mb-4"
+              onClick={() => setIsMenuOpen(false)}
+              key={route.name}
+            >
+              <Link className="hover:underline" href={route.path}>
+                {route.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
