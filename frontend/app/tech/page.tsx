fe3c03843626310f3e6244787caa0d84fe974eef
Christian Rosenville <web.chrisr@gmail.com>
2024-11-28 22:29:00 +0100

    added tech page

---
+++ frontend/app/tech/page.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react";
+
+import { PageTitle } from "@/components/page-title/PageTitle";
+import { PaginationGrid } from "@/components/sections/pagination-grid/PaginationGrid";
+import { LoadingScreen } from "@/components/ui/loading/screen/LoadingScreen";
+
+export default async function TechPage() {
+  return <div>Reviews</div>;
+
+  // return (
+  //   <main>
+  //     <PageTitle
+  //       title="Reviews"
+  //       subtitle="Read our honest, detailed, and informative reviews of the best games in the market. This section is your guide to gaming."
+  //     />
+
+  //     <Suspense fallback={<LoadingScreen />}>
+  //       <PaginationGrid articles={articles.articles} />
+  //     </Suspense>
+  //   </main>
+  // );
+}
